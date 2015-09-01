401089d4a30465fd8a5fe41af9ae94789eb61be6
Nobuhiko Miyamoto <TyouKyozyakuTaisitu@yahoo.co.jp>
2015-09-02 01:41:15 +0900

    141

--- projects/Crawler/active.sh
+++ projects/Crawler/active.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 cd `dirname $0`
-rtact /localhost/Crawler.rtc
+rtact /localhost/MotorDriver_TA8428K0.rtc
+rtact /localhost/CrawlerController0.rtc
+rtact /localhost/AccAndMagnSensor_LSM303DLHC0.rtc
+rtact /localhost/RangeSensor_GP2Y0A21YK0.rtc
+rtact /localhost/RangeSensor_GP2Y0A21YK1.rtc
+rtact /localhost/RangeSensor_GP2Y0A21YK2.rtc
+rtact /localhost/RangeSensor_GP2Y0A21YK3.rtc
+rtact /localhost/MotorDriver_TA8428K1.rtc
--- projects/Crawler/deactive.sh
+++ projects/Crawler/deactive.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 cd `dirname $0`
-rtdeact /localhost/Crawler.rtc
+rtdeact /localhost/MotorDriver_TA8428K0.rtc
+rtdeact /localhost/CrawlerController0.rtc
+rtdeact /localhost/AccAndMagnSensor_LSM303DLHC0.rtc
+rtdeact /localhost/RangeSensor_GP2Y0A21YK0.rtc
+rtdeact /localhost/RangeSensor_GP2Y0A21YK1.rtc
+rtdeact /localhost/RangeSensor_GP2Y0A21YK2.rtc
+rtdeact /localhost/RangeSensor_GP2Y0A21YK3.rtc
+rtdeact /localhost/MotorDriver_TA8428K1.rtc
